fe6c9b2cfb1d20b34a76b11da058cef94c7c8d91
unmonoqueteclea <gonzalez@bigml.com>
2022-09-12 17:40:53 +0200

    feat: add node version manager

--- shell/.bash/dev.sh
+++ shell/.bash/dev.sh
@@ -22,3 +22,7 @@ bigml_docker_login() {
     echo $DOCKER_BIGML_PASSWORD | docker login -u $DOCKER_BIGML_USERNAME --password-stdin 
 }
 
+# configure node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
