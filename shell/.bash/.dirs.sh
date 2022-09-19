efecccec61c93429f1b222623ec2eac7b8a9fb34
unmonoqueteclea <gonzalez@bigml.com>
2022-09-19 08:11:02 +0200

    feat: add alias to reconnect rclone

--- shell/.bash/.dirs.sh
+++ shell/.bash/.dirs.sh
@@ -5,8 +5,9 @@
 # this script exports variables with the location of some useful
 # directories
 
-# Google Drive folder is mounted, with crontab, on init. Use this
-# command to umount it
+# Google Drive folder is usually mounted with crontab on init.
+alias rclone-reconnect="rclone config reconnect drive:"
+alias drive-mount="rclone mount drive: ${HOME}/Drive/  --vfs-cache-mode full --file-perms 0600 --daemon"
 alias drive-umount="sudo umount -f ${HOME}/Drive/"
 
 # folder that stores version-controlled projects
