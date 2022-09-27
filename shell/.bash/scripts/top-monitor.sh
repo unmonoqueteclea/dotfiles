3d0aa957fe9fcf6ed2ec0fc02b3a8b93bfdf3682
unmonoqueteclea <gonzalez@bigml.com>
2022-09-27 13:39:18 +0200

    feat: add top-monitor script

--- shell/.bash/scripts/top-monitor.sh
+++ shell/.bash/scripts/top-monitor.sh
@@ -3,52 +3,71 @@ set -Eeuo pipefail
 
 trap cleanup SIGINT SIGTERM ERR EXIT
 
-# script logic
+# find script location, so that we can create paths like this:
+# cat "$script_dir/my_file"
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cpu_general() {
+    top -bn1 | grep "%Cpu(s)" | cut -d, -f 1 | tr -d '%Cpu(s): ' | tr -d ' us'
+}
+
+cpu_pid() {
+    top -bn1 -p $pid | tail -n +8 | tr -s ' ' | cut -d ' ' -f 10
+}
+
+mem_general() {
+    top -bn1 | grep "MiB Mem" | cut -d, -f 1 | tr -d 'MiB Mem : ' | tr -d ' total'
+}
+
+swap_general() {
+    top -bn1 | grep "MiB Swap" | cut -d, -f 1 | tr -d 'MiB Swap : ' | tr -d ' total'
+}
+
+mem_pid() {
+    top -bn1 -p $pid | tail -n +8 | tr -s ' ' | cut -d ' ' -f 11
+}
+
 collect() {
-    if [ "$cpu" -eq 1 ];
-    then
-	while [[ ($time -lt 0) || ($SECONDS -lt $time) ]];
-	do
-	    docker stats --no-stream |
-		grep ${container} |
-		awk  -v date="$(date +%T)" '{OFS=","};{print date, $3}'|
-		sed -e 's/%//g' >> ${file};
-	done
-    else
-	while [[ ($time -lt 0) || ($SECONDS -lt $time) ]];
-	do
-	    docker stats --no-stream |
-		grep ${container} |
-		awk -v date="$(date +%T)" '{OFS=","}{ if(index($4, "GiB")) {gsub("GiB","",$4); print date, $4*1000} else {gsub("MiB","",$4); print date,$4}}' >> ${file};
-	done
-    fi
+    while [[ ("$time" -lt 0) || ("$SECONDS" -lt "$time") ]];
+    do
+	timenow=$(date +%T);
+	if [ ${cpu} -eq 1 ];
+	then
+	    val=$([ ${pid} -eq -1 ] && cpu_general || cpu_pid)
+	    echo "$timenow,$val" >> $file
+	else
+	    val=$([ ${pid} -eq -1 ] && mem_general || mem_pid)
+	    swap=$([ ${pid} -eq -1 ] && swap_general || echo '0')
+	    echo "$timenow,$val,$swap" >> $file
+	fi
+	sleep $period
+    done
 }
 
 main() {
-  # modified from https://www.zakariaamine.com/2019-12-04/monitoring-docker
-  container=${args[0]-}
-  file=${script_dir}/stats-${container}.csv
-  gnufile=${script_dir}/stats-${container}.gnuplot
-
-  if [ "$plot" -eq 1 ];
+  file=${script_dir}/top-stats.csv
+  gnufile=${script_dir}/top-stats.gnuplot
+  if [ ${plot} -eq 1 ];
   then
-      # create gnuplot file
       touch ${gnufile}
       if test -f "$gnufile"; then rm ${gnufile}; fi
+      echo 'set xtics font ", 3"' >> ${gnufile}
+      echo 'set ytics font ", 4"' >> ${gnufile}
+      echo 'set key font ",4"' >> ${gnufile}
       echo "set datafile separator ','" >> ${gnufile}
       echo "set xdata time # tells gnuplot the x axis is time data" >> ${gnufile}
       echo "set timefmt \"%H:%M:%S\" # specify our time string format" >> ${gnufile}
       echo "set format x \"%H:%M:%S\" # otherwise it will show only MM:SS" >> ${gnufile}
       echo "plot \"${file}\" using 1:2 with lines "  >> ${gnufile}
   fi
-  
   if test -f "$file"; then
-      msg "${RED} File ${file} already exists. It will be overwritten ${NOFORMAT}"
+      msg "${RED} File ${file} already exists. ${NOFORMAT}"
+      msg "${RED} File will be overwritten. ${NOFORMAT}"
       rm ${file} ;
   fi
   msg ""
   filetext="${GREEN}${file}${NOFORMAT}"
-  msg "Capturing stats from container ${BLUE}${container}${NOFORMAT} to ${filetext}"
+  msg "Capturing stats from  ${BLUE}top${NOFORMAT} to ${filetext}"
   msg "Press C-c to stop..."
   msg ""
   collect
@@ -65,30 +84,26 @@ report_results() {
     fi
 }
 
-# find script location, so that we can create paths like this:
-# cat "$script_dir/my_file"
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
 usage() {
   cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") -c|-m [-h] [-v] [-V] [-q] [-t secs] container_name
-Collect docker stats values for a specific container and store them
-in a text file.
+Usage: $(basename "${BASH_SOURCE[0]}") -c|-m [-h] [-V] [-q] [-t secs] [-T period] [-pid pid]
+Collect user CPU or free memory from 'top' and store the values in a text file.
 
 Available options:
 -h, --help      Print this help and exit
--v, --verbose   Print script debug info
 -V, --version   Show script version
--c, --cpu      	Collect CPU usage values
+-c, --cpu      	Collect user CPU usage values (in %)
 -p, --plot      Plot values after finish collection
--m, --memory  	Collect memory usage values
+--pid           Collect stats for a specific PID
+-m, --memory  	Collect free memory values (in MB)
 -q, --quiet     Don't show status messages
 -t, --time      Total collection seconds (default: infinite)
+-T, --period    Collection period in seconds (default: 1)
 EOF
   exit
 }
 
-version() { 
+version() {
     echo "$(basename "${BASH_SOURCE[0]}") 0.0.1"
     exit
 }
@@ -96,7 +111,6 @@ version() {
 # like of a finally block for the script
 cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
   if [ "$plot" -eq 1 ];
   then
       $(gnuplot -p ${gnufile})
@@ -118,7 +132,7 @@ msg() {
   echo >&2 -e "${1-}"
 }
 
-die() {    
+die() {
   local msg=$1
   local code=${2-1} # default exit status 1
   msg "$msg"
@@ -126,13 +140,13 @@ die() {
 }
 
 parse_params() {
-    # default values of variables set from params
   cpu=0
   memory=0
   plot=0
   quiet=0
   time=-1
-  
+  pid=-1
+  period=1
 
   while :; do
     case "${1-}" in
@@ -143,9 +157,15 @@ parse_params() {
     -c | --cpu) cpu=1 ;;
     -m | --memory) memory=1 ;;
     -p | --plot) plot=1 ;;
-    -q | --quiet) quiet=1 ;;
+    -q | --quiet) quiet=1;;
+    -T | --period)
+	period="${2-}"
+	shift ;;
     -t | --time)
 	time="${2-}"
+	shift ;;
+    --pid)
+	pid="${2-}"
 	shift ;;
     -?*) die "Unknown option: $1" ;;
     *) break ;;
@@ -154,11 +174,8 @@ parse_params() {
   done
 
   args=("$@")
-
-  [[ ${#args[@]} -eq 0 ]] && msg "Missing script arguments" && usage
   [[ ${cpu} -eq 1 ]] && [[ ${memory} -eq 1 ]]  && die "Choose only one: cpu or memory"
   [[ ${cpu} -eq 0 ]] && [[ ${memory} -eq 0 ]]  && die "Choose one flag: cpu or memory"
-  
   return 0
 }
 
