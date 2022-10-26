6fdcfc9411c9dab071f7adc7777e5ce8cedf3041
unmonoqueteclea <gonzalez@bigml.com>
2022-10-26 08:24:49 +0200

    fix: optimize rclone synchronization

--- shell/.bash/.dirs.sh
+++ shell/.bash/.dirs.sh
@@ -8,7 +8,10 @@
 # Google Drive folder is usually mounted with crontab on init.
 export DIR_SYNC="${HOME}/Drive"
 alias drive-reconnect="rclone config reconnect drive:"
-alias drive-mount="rclone mount drive: ${DIR_SYNC}/  --vfs-cache-mode full --file-perms 0600 --daemon"
+# we use attr-timeout to cache attributes. We configured a high value, it can cause
+# corruption if files changes on the remote (in my case, that is not likely to happen)
+# see https://rclone.org/commands/rclone_mount/#attribute-caching
+alias drive-mount="rclone mount drive: ${DIR_SYNC}/  --attr-timeout 5m --vfs-cache-mode full --vfs-cache-max-age 5h  --dir-cache-time 15m --poll-interval 5m --file-perms 0600 --vfs-write-back 10m --daemon"
 alias drive-umount="sudo umount -f ${DIR_SYNC}/"
 
 # folder that stores version-controlled projects
