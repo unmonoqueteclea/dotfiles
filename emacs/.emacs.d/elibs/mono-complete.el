52b5ce11f0fd1e0c974a10b557de81dfcb1103df
unmonoqueteclea <gonzalez@bigml.com>
2022-11-16 11:18:07 +0100

    feat[emacs]: remove python coverage folder from ripgrep

--- emacs/.emacs.d/elibs/mono-complete.el
+++ emacs/.emacs.d/elibs/mono-complete.el
@@ -124,7 +124,7 @@
 
 (setq consult-ripgrep-args
       "rg --null -uu --line-buffered --color=never --max-columns=1000
-          --glob !{.git,.mypy_cache,.egg-info,.pytest_cache,*.egg-info,node_modules/*}
+          --glob !{cov_html,.git,.mypy_cache,.egg-info,.pytest_cache,*.egg-info,node_modules/*}
           --path-separator /   --smart-case --no-heading --line-number .")
 
 
