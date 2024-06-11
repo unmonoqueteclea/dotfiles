83520419784d9bd29b1fa215b84e34ec5147ba77
unmonoqueteclea <gonzalez@bigml.com>
2024-06-11 10:22:11 +0200

    [emacs]: add more dired omit files

--- emacs/.emacs.d/elibs/mono-files.el
+++ emacs/.emacs.d/elibs/mono-files.el
@@ -40,7 +40,7 @@
 (require 'dired-x)
 (add-hook 'dired-mode-hook #'dired-omit-mode)
 (setq dired-omit-files
-      "^.ipynb_checkpoints$\\|^.DS_Store$\\|^.ruff_cache$\\|^.git$\\|.egg-info$\\|.pytest_cache$\\|__pycache__$\\|Icon\015$\\|.mypy_cache$")
+      "^.ipynb_checkpoints$\\|^.DS_Store$\\|^.ruff_cache$\\|^.git$\\|.egg-info$\\|.pytest_cache$\\|__pycache__$\\|Icon\015$\\|.mypy_cache$\\|^.clerk$\\|^.cpcache$\\|^.nrepl-port$")
 
 ;; ls --help to show switches
 (setq dired-listing-switches "-alGh")
